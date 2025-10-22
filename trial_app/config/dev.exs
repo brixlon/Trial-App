367f115e4858b2ce787711a5dba5493a8741439a
brixlon <benmokua3@gmail.com>
2025-10-22 11:29:55 +0300

    commit

--- trial_app/config/dev.exs
+++ trial_app/config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :trial_app, TrialApp.Repo,
   username: "postgres",
-  password: "ddeveloper",
+  password: "developer",
   hostname: "localhost",
   database: "trial_app_dev",
   stacktrace: true,
